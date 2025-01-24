3e2e229415104ee6320f9881dfa3518f9b3f51e6
nidhiwankhede19 <nidhiwankhede19@gmail.com>
2025-01-24 19:38:28 +0530

    CHORE: tailwind color fixes

--- src/lib/App.tsx
+++ src/lib/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Button from "../components/Button";
 import { toLoweercase } from "../utils/toLowercase";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/src/components/ui/accordion";
 
 function App() {
   return (
     <div>
-      <h1 className="p-5">Hello 2</h1>
+      <h1 className="p-5 text-[7rem] bg-light-bg">Hello 2</h1>
       <Button>Heyy</Button>
       <p>{toLoweercase("HHELLO")}</p>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
