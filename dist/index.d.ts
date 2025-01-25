33f304402cf85d9da82b13bbda41b1796a4e8dc4
prtk-kry <tech1@kraya.co>
2025-01-25 12:30:32 +0530

    FIX: Removed dist from git tracking

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "@/components/Button";
-export * from "@/components/common/TextTruncate";
-export * from "@/lib/utils";
--- dist/utils/toLowercase.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function toLoweercase(str: string): string;
