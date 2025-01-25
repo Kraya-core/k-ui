7af8fcb9314888bcb69f34aaf6dedd2adcbd7da1
prtk-kry <tech1@kraya.co>
2025-01-25 11:35:13 +0530

    FIX: Updated exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
-export { default as Button } from "./Button";
-export { default as TextTruncate } from "@/components/common/TextTruncate";
+
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export * from "@/components";
+export { default as Button } from "@/components/Button";
+export { default as TextTruncate } from "@/components/common/TextTruncate";
 export * from "@/lib/utils";
