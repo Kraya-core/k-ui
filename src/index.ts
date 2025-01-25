16bd3cf727a604cb06e4015cdf4059bc3cd32613
prtk-kry <tech1@kraya.co>
2025-01-25 23:56:00 +0530

    FIX

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as Button } from "./components/Button";
-export { default as TextTruncate } from "./components/common/TextTruncate";
-export { default as TextTruncate2 } from "@/components/common/TextTruncate";
+export { default as Button } from "@/components/Button";
+export { default as TextTruncate } from "@/components/common/TextTruncate";
 export * from "@/lib/utils";
