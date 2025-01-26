cf1a5fdc33a86132f2503d0a31d78243fd1e1f2b
prtk-kry <tech1@kraya.co>
2025-01-26 22:07:59 +0530

    FIX: updated imports

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-import { Loader } from "@/ui-kit/loader/Loader";
+import Loader from "@/ui-kit/loader/Loader";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-[0.7rem] 2xl:text-[0.8rem] font-medium ring-offset-background transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
