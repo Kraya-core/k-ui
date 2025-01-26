58dd57811abc90c66e92554985a362619db5502a
prtk-kry <tech1@kraya.co>
2025-01-26 11:02:54 +0530

    FIXES

--- rollup.config.js
+++ rollup.config.js
@@ -1,58 +1,73 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+// import resolve from "@rollup/plugin-node-resolve";
+// import commonjs from "@rollup/plugin-commonjs";
+// import typescript from "@rollup/plugin-typescript";
+// import peerDepsExternal from "rollup-plugin-peer-deps-external";
+// import postcss from "rollup-plugin-postcss";
+
+// export default {
+//   input: "src/index.ts",
+//   output: [
+//     {
+//       file: "dist/index.js",
+//       format: "cjs",
+//       sourcemap: true,
+//     },
+//     {
+//       file: "dist/index.esm.js",
+//       format: "esm",
+//       sourcemap: true,
+//     },
+//   ],
+//   plugins: [
+//     peerDepsExternal(),
+//     resolve(),
+//     commonjs(),
+//     typescript({ tsconfig: "./tsconfig.json" }),
+//     postcss(),
+//   ],
+//   external: ["react", "react-dom"],
+// };
+
 import typescript from "@rollup/plugin-typescript";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
+import alias from "@rollup/plugin-alias";
+import { dts } from "rollup-plugin-dts";
+import path from "path";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    postcss(),
-  ],
-  external: ["react", "react-dom"],
-};
-
-// const config = [
-//   {
-//     input: "src/index.ts",
-//     output: [
-//       {
-//         file: "dist/index.js",
-//         format: "cjs",
-//         sourcemap: true,
-//       },
-//       {
-//         file: "dist/index.esm.js",
-//         format: "esm",
-//         sourcemap: true,
-//       },
-//     ],
-//     plugins: [
-//       peerDepsExternal(),
-//       resolve(),
-//       commonjs(),
-//       typescript({ tsconfig: "./tsconfig.json" }),
-//       postcss(),
-//       dts(),
-//     ],
-//     external: ["react", "react-dom"],
-//   },
-// ];
-
-// export default config;
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      alias({
+        entries: [
+          {
+            find: "@components",
+            replacement: path.resolve(__dirname, "src/components"),
+          },
+        ],
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: true,
+        declarationDir: "dist/types",
+      }),
+    ],
+    external: ["react", "react-dom"], // Exclude peer dependencies
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
