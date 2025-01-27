4f3d9973f0d0d79b1fedd7bc3fd262d9f0136f71
prtk-kry <tech1@kraya.co>
2025-01-27 12:11:18 +0530

    FIX: removed RHFDatePicker due to import issues of react-day-picker and fixed import statement

--- src/lib/App.tsx
+++ src/lib/App.tsx
@@ -5,20 +5,39 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { z } from "zod";
+import { RHFForm, RHFInput, RHFPasswordInput } from "@/ui-kit";
+import { Button } from "@/components/ui/button";
 
 function App() {
+  const defaultValues = {
+    name: "",
+    password: "",
+  };
+
+  const validationSchema = z.object({
+    name: z.string().min(1, { message: "This is a required field" }),
+    password: z.string().min(1, { message: "This is a required field" }),
+  });
+
+  const onSubmit = (values: typeof defaultValues) => {
+    console.log({ values });
+  };
+
   return (
     <div className="p-5">
       <h1 className="p-5 text-[7rem] bg-light-bg">Hello 2</h1>
-
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <div>
+        <RHFForm
+          defaultValues={defaultValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          <RHFInput label="Name" name="name" />
+          <RHFPasswordInput label="Password" name="password" />
+          <Button type="submit">Submit</Button>
+        </RHFForm>
+      </div>
     </div>
   );
 }
--- src/ui-kit/index.ts
+++ src/ui-kit/index.ts
@@ -1,2 +1,18 @@
 export { default as AsyncButton } from "@/ui-kit/buttons/AsyncButton";
 export { default as Loader } from "@/ui-kit/loader/Loader";
+
+// Form
+export { default as RHFArrayField } from "@/ui-kit/form/RHFArrayField";
+export { default as RHFCheckbox } from "@/ui-kit/form/RHFCheckbox";
+// export { default as RHFDatePicker } from "@/ui-kit/form/RHFDatePicker";
+export { default as RHFForm } from "@/ui-kit/form/RHFForm";
+export { default as RHFInput } from "@/ui-kit/form/RHFInput";
+export { default as RHFKeywords } from "@/ui-kit/form/RHFKeywords";
+export { default as RHFMultiSelect } from "@/ui-kit/form/RHFMultiSelect";
+export { default as RHFNumericInput } from "@/ui-kit/form/RHFNumericInput";
+export { default as RHFPasswordInput } from "@/ui-kit/form/RHFPasswordInput";
+export { default as RHFPhoneNumberInput } from "@/ui-kit/form/RHFPhoneNumberInput";
+export { default as RHFRadio } from "@/ui-kit/form/RHFRadio";
+export { default as RHFSingleSelect } from "@/ui-kit/form/RHFSingleSelect";
+export { default as RHFSwitch } from "@/ui-kit/form/RHFSwitch";
+export { default as RHFTextarea } from "@/ui-kit/form/RHFTextarea";
